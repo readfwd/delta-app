88525f5befe856c14292f0876693f566cc9ca54d
Theodor Văraru <theo@vararu.org>
2014-08-06 14:46:35 +0300

    Finish initial scrollgrid example.

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,12 @@ require('famous-polyfills');
 var _ = require('lodash');
 var Engine = require('famous/core/Engine');
 var Surface = require('famous/core/Surface');
+var ImageSurface = require('famous/surfaces/ImageSurface');
+var RenderNode = require('famous/core/RenderNode');
 var Modifier = require('famous/core/Modifier');
 var Transform = require('famous/core/Transform');
-var GridLayout = require("famous/views/GridLayout");
+var GridLayout = require('famous/views/GridLayout');
+var ScrollView = require('famous/views/ScrollView');
 
 module.exports = {
   launch: _.once(function () {
@@ -16,27 +19,51 @@ module.exports = {
 
     var mainContext = Engine.createContext();
 
+    var scrollView = new ScrollView();
+
+    var numSurfaces = 24;
+    var height = 300;
+
     var grid = new GridLayout({
-      dimensions: [4, 2]
+      dimensions: [2, numSurfaces / 2]
     });
 
     var surfaces = [];
     grid.sequenceFrom(surfaces);
 
-    for (var i = 0; i < 8; i++) {
-      surfaces.push(new Surface({
-        content: 'panel ' + (i + 1),
-        size: [undefined, undefined],
-        properties: {
-          backgroundColor: 'hsl(' + (i * 360 / 8) + ', 100%, 50%)',
-          color: '#333',
-          lineHeight: '100px',
-          textAlign: 'center'
+
+    var tempSurface, tempMod, tempNode, initialTime = Date.now();
+    for (var i = 0; i < numSurfaces; i++) {
+      tempSurface = new ImageSurface({
+        content: 'http://placekitten.com/1000/10' + ((i < 10) ? ('0' + i) : i),
+        size: [undefined, height],
+        classes: ['backfaceVisibility']
+      });
+
+
+      tempMod = new Modifier({
+        origin: [0.5, 0.5],
+        transform: function () {
+          return Transform.rotateY(0.002 * (Date.now() - initialTime));
         }
-      }));
+      });
+      
+      tempSurface.pipe(scrollView);
+
+      tempNode = new RenderNode();
+      tempNode.add(tempMod).add(tempSurface);
+      surfaces.push(tempNode);
     }
 
-    mainContext.add(grid);
+    var gridModifier = new Modifier({
+      size: [undefined, (numSurfaces * height) / 2]
+    });
+    var renderNode = new RenderNode();
+    renderNode.add(gridModifier).add(grid);
+
+    scrollView.sequenceFrom([renderNode]);
+
+    mainContext.add(scrollView);
   })
 };
 
