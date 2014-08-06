fea5e4c1870c1e5dd532127dd863d7af4ef24279
Theodor Văraru <theo@vararu.org>
2014-08-06 13:12:48 +0300

    Basic Famous demo.

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,7 @@ var _ = require('lodash');
 var Engine = require('famous/core/Engine');
 var Surface = require('famous/core/Surface');
 var Modifier = require('famous/core/Modifier');
+var Transform = require('famous/core/Transform');
 
 module.exports = {
   launch: _.once(function () {
@@ -20,11 +21,16 @@ module.exports = {
         textAlign: 'center',
         lineHeight: '200px',
         background: '#ddd',
-      }
+      },
+      classes: ['backfaceVisibility']
     });
 
+    var initialTime = Date.now();
     var modifier = new Modifier({
-      origin: [0.5, 0.5]
+      origin: [0.5, 0.5],
+      transform: function () {
+        return Transform.rotateY(0.002 * (Date.now() - initialTime));
+      }
     });
 
     mainContext.add(modifier).add(surface);
