36cc29c9849cdc32acffaf4f5fe33a5943b07301
Theodor Văraru <theo@vararu.org>
2014-08-07 11:44:30 +0300

    Remove cordova test code.

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,6 @@
 require('famous-polyfills');
 
 var _ = require('lodash');
-var $ = require('jquery');
 var cordova = require('./cordova-shim');
 
 module.exports = {
@@ -11,12 +10,6 @@ module.exports = {
     var self = this;
     cordova.ready(function() {
       window.app = self;
-      document.title = 'Altceva!';
-      if (cordova.isMobile) {
-        $('body').append('<h1>cordova works</h1>');
-      } else {
-        $('body').append('<h1>cordova shim works</h1>');
-      }
     });
   })
 };
