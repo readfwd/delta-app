2be9ae9a4a6878ddcb0e54d76728f079637a3212
Marius Petcu <dapetcu21@gmail.com>
2014-08-07 12:50:12 +0300

    Added build-deps script for CrossWalk

---
+++ mobile/build_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+cd "$SCRIPTPATH/platforms/android/CordovaLib/"
+android update project --subprojects --path . --target "android-19"
+ant debug
+ant release
