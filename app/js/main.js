29c7541026b149b634dc550691c8e6919c47147a
Marius Petcu <dapetcu21@gmail.com>
2014-08-08 13:39:26 +0300

    Local maps

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,7 @@ require('famous-polyfills');
 var _ = require('lodash');
 var cordova = require('./cordova-shim');
 var ol = require('./ol.js');
+var $ = require('jquery');
 
 module.exports = {
   launch: _.once(function () {
@@ -12,16 +13,29 @@ module.exports = {
     cordova.ready(function() {
       window.app = self;
 
+      $('body').append('<div id="map"></div>');
+
+      var extent = ol.proj.transformExtent([28.5, 44.33, 29.83, 45.6], 'EPSG:4326', 'EPSG:3857');
+
       var map = new ol.Map({
           target: 'map',
         layers: [
           new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'sat'})
+            source: new ol.source.XYZ({
+              attributions: [
+                ol.source.OSM.DATA_ATTRIBUTION
+              ],
+              url: 'assets/maps/delta/{z}/{x}/{y}.png'
+            }),
+            extent: extent
           })
         ],
         view: new ol.View({
-          center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 4
+          extent: extent,
+          center: ol.proj.transform([29.165, 44.965], 'EPSG:4326', 'EPSG:3857'),
+          minZoom: 8,
+          maxZoom: 15,
+          zoom: 9
         })
       });
 
