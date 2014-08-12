06e4102a767cdce39c3547e4f3bb29b4e8b7f6a9
Theodor Văraru <theo@vararu.org>
2014-08-12 11:47:36 +0300

    Lint/review some code in menu-controller.js

--- app/js/controllers/menu-controller.js
+++ app/js/controllers/menu-controller.js
@@ -5,7 +5,6 @@ var GridLayout = require('famous/views/GridLayout');
 var Surface = require('famous/core/Surface');
 var Modifier = require('famous/core/Modifier');
 var RenderNode = require('famous/core/RenderNode');
-var assert = require('assert');
 var RenderController = require('famous/views/RenderController');
 var Transform = require('famous/core/Transform');
 var Transitionable = require('famous/transitions/Transitionable');
@@ -14,8 +13,9 @@ var SpringTransition = require('famous/transitions/SpringTransition');
 Transitionable.registerMethod('spring', SpringTransition);
 
 function MenuController() {
+
 }
-util.inherits(MenuController, ViewController)
+util.inherits(MenuController, ViewController);
 
 MenuController.prototype.loadView = function () {
   var self = this;
@@ -36,7 +36,7 @@ MenuController.prototype.loadView = function () {
   })).add(grid);
 
   self.view = node;
-  
+
   var renderController = new RenderController({
     inTransition: {
       method: 'spring',
@@ -76,22 +76,22 @@ MenuController.prototype.loadView = function () {
 
   renderController.show(renderNodes[0]);
 
-  for (var i = 0; i < 4; i++) {
-    (function(i) {
-      var surface = new Surface({
-        content: i + 1,
-        size: [undefined, undefined],
-        classes: ['btn']
-      });
-      surfaces.push(surface);
+  var showSurface = function (i) {
+    return function () {
+      renderController.show(renderNodes[i]);
+    };
+  };
 
-      surface.on('click', function(evt) {
-        renderController.show(renderNodes[i]);
-      });
-    })(i);
+  for (var i = 0; i < 4; i++) {
+    var surface = new Surface({
+      content: i + 1,
+      size: [undefined, undefined]
+    });
+    surfaces.push(surface);
+
+    surface.on('click', showSurface(i));
   }
 
-  //var inTransitionable = new Transitionable(Tranform.translate(-320, 0, 0));
   renderController.inTransformFrom(function (progress) {
     return Transform.translate(window.innerWidth * (progress - 1), 0, 0);
   });
@@ -99,14 +99,14 @@ MenuController.prototype.loadView = function () {
     return Transform.translate(window.innerWidth * (progress - 1), 0, 0);
   });
 
-  renderController.inOpacityFrom(function (progress) {
+  renderController.inOpacityFrom(function () {
     return 1;
   });
-  renderController.outOpacityFrom(function (progress) {
+  renderController.outOpacityFrom(function () {
     return 1;
   });
 
   node.add(renderController);
-}
+};
 
 module.exports = MenuController;
