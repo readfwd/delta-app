6ce461ce2d83e1272a9eb96826bca0c015c0ddd6
Marius Petcu <dapetcu21@gmail.com>
2014-08-20 22:01:33 +0300

    Master-detail prototype

---
+++ app/js/controllers/master-controller.js
@@ -0,0 +1,81 @@
+var TemplateController = require('./template-controller');
+var DetailController = require('./detail-controller');
+var util = require('util');
+var Famous = require('../shims/famous');
+var _ = require('lodash');
+var $ = require('jquery');
+var Templates = require('../lib/templates');
+
+function MasterController(options) {
+  TemplateController.call(this, options);
+}
+util.inherits(MasterController, TemplateController);
+
+MasterController.prototype.prepareDetailController = function () {
+  var self = this;
+
+  if (!self.detailController) {
+    self.detailController = new DetailController({
+      templates: self.templates,
+      titles: self.titles,
+      titleBar: self.titleBar,
+    });
+    self.detailController.on('back', function () {
+      self.emit('ascend');
+    });
+  }
+};
+
+MasterController.prototype.navigateToIndex = function (index) {
+  var self = this;
+
+  self.prepareDetailController();
+
+  if (self.viewController) {
+    self.detailController.navigateToIndex(index, true);
+  } else {
+    self.detailController.navigateToIndex(index, false);
+    self.setNavigationItem(self.detailController);
+    self.emit('descend');
+  }
+};
+
+MasterController.prototype.setUpPage = function (page) {
+  var self = this;
+
+  var links = page.find('a');
+  links.on('click', function (evt) {
+    evt.preventDefault();
+  });
+
+  var templates = [];
+  var titles = [];
+
+  links.each(function (idx, el) {
+    var $el = $(el);
+
+    var href = $el.attr('href');
+    var template = Templates;
+    _.each(href.split('/'), function (el) {
+      template = template[el];
+    });
+    templates.push(template);
+
+    titles.push($el.data('title'));
+
+    $el.on('click', function (evt) {
+      evt.preventDefault();
+    });
+
+    Famous.FastClick($el, function() {
+      self.navigateToIndex(idx);
+    });
+  });
+
+  if (!self.templates) {
+    self.templates = templates;
+    self.titles = titles;
+  }
+};
+
+module.exports = MasterController;
--- app/js/controllers/titlebar-controller.js
+++ app/js/controllers/titlebar-controller.js
@@ -8,6 +8,9 @@ function TitleBarController(options) {
   options = options || {};
   options.title = options.title || '';
   options.backIcon = options.backIcon || 'fa-chevron-left';
+  if (options.pushTitleBar === undefined) {
+    options.pushTitleBar = true;
+  }
   this.titleBar = options.titleBar;
   NavigationController.call(this, options);
 }
@@ -151,7 +154,9 @@ TitleBarController.prototype.buildRenderTree = function (parentNode) {
   self.contentModifier = contentModifier;
   self.contentShowModifier = contentShowModifier;
 
-  self.titleBar.pushItem(self.buildBarItem());
+  if (self.options.pushTitleBar) {
+    self.titleBar.pushItem(self.buildBarItem());
+  }
   self.buildContentTree(contentWrapper);
   self.buildNavRenderController(contentRoot);
 
