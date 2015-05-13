0d21eb59c513971a93cce56d2de4b5597a7c97aa
Marius Petcu <dapetcu21@gmail.com>
2015-05-13 13:02:12 +0300

    Updated cordova and crosswalk

--- mobile/build_deps.sh
+++ mobile/build_deps.sh
@@ -3,21 +3,22 @@
 set -e
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+APILEVEL="android-21"
 
 cd "$SCRIPTPATH"
 find platforms/android -type d \( -name ant-build -or -name ant-gen \) | xargs rm -r
 
 cd "$SCRIPTPATH/platforms/android/CordovaLib/"
-android update project --subprojects --path . --target "android-19"
+android update project --subprojects --path . --target $APILEVEL
 ant release
 cd "$SCRIPTPATH/platforms/android/google/play_licensing/library/"
-android update project --subprojects --path . --target "android-19"
+android update project --subprojects --path . --target $APILEVEL
 ant release
 cd "$SCRIPTPATH/platforms/android/google/play_apk_expansion/zip_file"
-android update project --subprojects --path . --target "android-19"
+android update project --subprojects --path . --target $APILEVEL
 ant release
 cd "$SCRIPTPATH/platforms/android/google/play_apk_expansion/downloader_library/"
-android update project --subprojects --path . --target "android-19"
+android update project --subprojects --path . --target $APILEVEL
 ant release
 cd "$SCRIPTPATH/platforms/android/"
-android update project --subprojects --path . --target "android-19"
+android update project --subprojects --path . --target $APILEVEL
